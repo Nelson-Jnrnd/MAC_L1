9fd006d8b3fb413c4bd66f34ccf57daddf547d31
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2022-03-13 17:58:18 +0100

    add ^the cool ones^ in the java app, had to modify them because they are so cool they didnt pass the test

--- src/main/java/ch/heig/mac/Requests.java
+++ src/main/java/ch/heig/mac/Requests.java
@@ -33,7 +33,13 @@ public class Requests {
     }
 
     public List<JsonObject> hiddenGem() {
-        throw new UnsupportedOperationException("Not implemented, yet");
+        QueryResult result = cluster.query(
+                "SELECT title \n" +
+                        "FROM `mflix-sample`._default.`movies`\n" +
+                        "WHERE tomatoes.critic.meter = 100\n" +
+                        "AND tomatoes.viewer.rating IS NOT VALUED"
+        );
+        return result.rowsAs(JsonObject.class);
     }
 
     public List<JsonObject> topReviewers() {
@@ -48,7 +54,13 @@ public class Requests {
     }
 
     public List<String> greatReviewers() {
-        throw new UnsupportedOperationException("Not implemented, yet");
+        QueryResult result = cluster.query(
+                "SELECT RAW name\n" +
+                        "FROM `mflix-sample`._default.`comments`\n" +
+                        "GROUP BY name\n" +
+                        "HAVING count(*) > 300"
+        );
+        return result.rowsAs(String.class);
     }
 
     public List<JsonObject> bestMoviesOfActor(String actor) {
@@ -62,7 +74,15 @@ public class Requests {
     }
 
     public List<JsonObject> plentifulDirectors() {
-        throw new UnsupportedOperationException("Not implemented, yet");
+        QueryResult result = cluster.query(
+                "SELECT directors AS director_name, COUNT(directors) AS count_film\n" +
+                        "FROM `mflix-sample`.`_default`.`movies`\n" +
+                        "UNNEST directors\n" +
+                        "GROUP BY directors\n" +
+                        "HAVING COUNT(directors) > 30\n" +
+                        "ORDER BY count_film DESC"
+        );
+        return result.rowsAs(JsonObject.class);
     }
 
     public List<JsonObject> confusingMovies() {
@@ -75,11 +95,25 @@ public class Requests {
     }
 
     public List<JsonObject> commentsOfDirector1(String director) {
-        throw new UnsupportedOperationException("Not implemented, yet");
+        QueryResult result = cluster.query(
+                "SELECT comments.text, comments.movie_id AS movie_id\n" +
+                        "FROM `mflix-sample`.`_default`.`movies`\n" +
+                        "INNER JOIN `mflix-sample`.`_default`.`comments` ON comments.movie_id = movies._id\n" +
+                        "WHERE \"" + director + "\" IN movies.directors"
+        );
+        return result.rowsAs(JsonObject.class);
     }
 
     public List<JsonObject> commentsOfDirector2(String director) {
-        throw new UnsupportedOperationException("Not implemented, yet");
+        QueryResult result = cluster.query(
+                "SELECT text, comments.movie_id AS movie_id\n" +
+                        "FROM `mflix-sample`.`_default`.`comments`\n" +
+                        "WHERE comments.movie_id IN (\n" +
+                        "    SELECT RAW _id\n" +
+                        "    FROM `mflix-sample`.`_default`.`movies`\n" +
+                        "    WHERE \"" + director + "\" IN directors)"
+        );
+        return result.rowsAs(JsonObject.class);
     }
 
     // Returns the number of documents updated.
